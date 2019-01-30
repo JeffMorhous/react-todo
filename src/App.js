c6e52075564f04368252d9c84a7ed280191bd692
Jeff Morhous <jobboy18@gmail.com>
2019-01-30 00:58:28 -0500

    Add todos component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import todos from './components/todos';
 
 class App extends Component {
+  state ={
+    todos:[
+      {
+        id:1,
+        title:'Take out the trash',
+        completed:'false'
+      }
+    ]
+  }
   render() {
     return (
       <div className="App">
         <h1>Todo React Application</h1>
+        <Todos/>
       </div>
     );
   }
---
+++ src/components/todos.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Todo React Application</h1>
+      </div>
+    );
+  }
+}
+
+export default Todos;
